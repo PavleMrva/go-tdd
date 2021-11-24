4845fc278f50ce435d047fbfe6988d076fa21149
Pavle <pavlemrvaljevic1@gmail.com>
2021-11-24 10:59:07 +0100

    sync

---
+++ sync/sync_test.go
@@ -0,0 +1,61 @@
+package sync_learn
+
+import (
+	"testing"
+	"sync"
+)
+
+type Counter struct {
+	mu sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got Counter, count int) {
+	t.Helper()
+	if got.Value() != count {
+		t.Errorf("got %d, want %d", got.Value(), count)
+	}
+}
